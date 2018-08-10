f0bd9a65b9e1f7890a8f8791cc488afe220213bc
lushuang <lushuang3087@163.com>
2018-08-10 18:30:11 +0800

    整合hysitrix

---
+++ microservice-customer-movie-ribbon-with-hystrix-propagation/src/main/java/com/lushuang/cloud/controller/MovieController.java
@@ -0,0 +1,36 @@
+package com.lushuang.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.lushuang.cloud.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+@RestController
+public class MovieController {
+
+	@Autowired
+	private RestTemplate restTempldate;
+
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+
+	@GetMapping("/movie/{id}")
+	@HystrixCommand(fallbackMethod = "findByIdFallback", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE")
+	})
+	public User findById(@PathVariable("id") long id) {
+		return restTempldate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
+	}
+
+	public User findByIdFallback(long id) {
+		User user = new User();
+		user.setId(0L);
+		return user;
+	}
+}
