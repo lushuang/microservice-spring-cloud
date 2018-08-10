b739dcfaecaf810cee31e7a36ff93e2b90d2cbbc
lushuang <lushuang3087@163.com>
2018-08-10 20:58:02 +0800

    feign 整合 hystrix

---
+++ microservice-customer-movie-feign-with-hystrix/src/main/java/com/lushuang/cloud/feign/HystrixClientFallback.java
@@ -0,0 +1,17 @@
+package com.lushuang.cloud.feign;
+
+import org.springframework.stereotype.Component;
+
+import com.lushuang.cloud.entity.User;
+
+@Component
+public class HystrixClientFallback implements UserFeignClient {
+
+	@Override
+	public User findById(Long id) {
+		User user = new User();
+		user.setId(0L);
+		return user;
+	}
+
+}
