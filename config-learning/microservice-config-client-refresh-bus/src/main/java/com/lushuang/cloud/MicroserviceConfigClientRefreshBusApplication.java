2b720351841435bf123a058a950f2d3928b1ee79
lushuang <lushuang3087@163.com>
2018-08-12 20:07:51 +0800

    Spring Cloud Bus

---
+++ config-learning/microservice-config-client-refresh-bus/src/main/java/com/lushuang/cloud/MicroserviceConfigClientRefreshBusApplication.java
@@ -0,0 +1,12 @@
+package com.lushuang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigClientRefreshBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigClientRefreshBusApplication.class, args);
+	}
+}
