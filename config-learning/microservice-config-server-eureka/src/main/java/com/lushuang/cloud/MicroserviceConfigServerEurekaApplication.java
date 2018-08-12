dbb1f00bbb32e9e1ca1c44a901d49845c1d1c09d
lushuang <lushuang3087@163.com>
2018-08-12 16:46:08 +0800

    Spring Cloud Config Server & Client with Eureka

---
+++ config-learning/microservice-config-server-eureka/src/main/java/com/lushuang/cloud/MicroserviceConfigServerEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.lushuang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class MicroserviceConfigServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigServerEurekaApplication.class, args);
+	}
+}
